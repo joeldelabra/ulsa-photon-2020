9c7e4b292ee6458be0b88a4f926d47c57638eeb6
joeldelabra <joeldelabra@gmail.com>
2020-11-12 21:44:36 -0700

    photon project 2020

--- Assets/Scripts/MenuView.cs
+++ Assets/Scripts/MenuView.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button btnCreateRoom;
+    [SerializeField] Button btnJoinGame;
+
+    void Awake()
     {
-        
+        btnCreateRoom.onClick.AddListener(CreateRoomClick);
+        btnJoinGame.onClick.AddListener(FindRoomClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateRoomClick()
     {
-        
+        Launcher.instance.CreateRoom();
+        Debug.Log("Room creation view");
+    }
+
+    void FindRoomClick()
+    {
+        Launcher.instance.FindRoomViewClick();
+        Debug.Log("Game search");
     }
 }
